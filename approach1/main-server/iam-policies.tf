61f0da59e2f3c50e4b91123b2f25755d049f827d
FG <felixgiftinfo@gmail.com>
2023-06-29 12:24:01 +0100

    Update iam-policies.tf

--- approach1/main-server/iam-policies.tf
+++ approach1/main-server/iam-policies.tf
@@ -2,22 +2,46 @@ resource "aws_iam_policy" "jenkins_ec2_policies" {
   name   = "JenkinsEc2_Policies"
   policy = <<EOF
 {
-	"Version": "2012-10-17",
-	"Statement": [
-		{
-			"Sid": "VisualEditor0",
-			"Effect": "Allow",
-			"Action": [
-				"ec2:*",
-				"route53:*",
-				"autoscaling:*",
-				"elasticloadbalancing:*",
-				"cloudwatch:*",
-				"s3:*"
-			],
-			"Resource": "*"
-		}
-	]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "ec2:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "elasticloadbalancing:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "cloudwatch:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "autoscaling:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:AWSServiceName": [
+                        "autoscaling.amazonaws.com",
+                        "ec2scheduled.amazonaws.com",
+                        "elasticloadbalancing.amazonaws.com",
+                        "spot.amazonaws.com",
+                        "spotfleet.amazonaws.com",
+                        "transitgateway.amazonaws.com"
+                    ]
+                }
+            }
+        }
+    ]
 }
 EOF
   tags = {
